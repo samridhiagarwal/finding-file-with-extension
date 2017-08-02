36c29e62599feb7b42020115961ee72c12e5e878
Samridhi Agarwal <samridhiagarwal855@gmail.com>
2017-08-02 20:40:12 +0530

    Find file by extension

---
+++ extension.py
@@ -0,0 +1,28 @@
+#project to extract all the files of certain extension from your computer and dump them in another folder.
+
+
+import os
+import fnmatch
+import glob
+import shutil
+path = ("C:\Users\Public\pytho\pyco")
+l = []
+flag = 0
+source_dir = ("C:\Users\Public\pytho\pyco")
+dest_dir = ("C:\Users\Public\pytho\destin")
+s = raw_input("Enter the extension to be moved in the format (*.extension): ")
+for root, dirs, files in os.walk(path):
+    for _file in files:
+        if fnmatch.fnmatch(_file,s):
+            l.append(os.path.join(root,_file))
+            flag = 1 
+            files = glob.iglob(os.path.join(source_dir,s))
+            #for file in files:
+                #if os.path.isfile(file):
+                    #shutil.copy(file, dest_dir)
+if flag == 0 :
+     print ("No Element Found")
+else:
+    for i in range (len(l)):
+        print i+1 , '.\t', l[i], '\n'
+        shutil.copy(l[i], dest_dir)
